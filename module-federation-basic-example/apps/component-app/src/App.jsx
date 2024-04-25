eaf2c49baac067d6a580bceb2b5fd7aa3c019b0f
sunny <seonyoung.jeon@injeinc.co.kr>
2024-04-25 15:38:21 +0900

    feat : 모듈 페더레이션 기본 테스트

---
+++ module-federation-basic-example/apps/component-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { createRoot } from "react-dom/client";
+
+import "./index.css";
+import Button from "./components/Button";
+
+const App = () => (
+  <div className="container">
+    <div>Name: component-app</div>
+    <div>Framework: react</div>
+    <div>Language: JavaScript</div>
+    <div>CSS: Empty CSS</div>
+    <Button onClick={() => alert("clicked")}>Click me..!!</Button>
+  </div>
+);
+createRoot(document.getElementById("app")).render(<App />);
