ee9300fcd10140d2a8f85c816cd07d314774976a
sunny <seonyoung.jeon@injeinc.co.kr>
2024-04-25 15:44:49 +0900

    feat : 모듈 페더레이션 : 다른 서버의 리액트 앱을 가져와서 엘리먼트에 랜더하기(격리)

---
+++ module-federation-isolated-example/apps/main-app/src/App.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import { createRoot } from "react-dom/client";
+
+import "./index.css";
+
+const elementId = "isolated-app";
+const App = () => {
+  useEffect(() => {
+    let unmount = () => {};
+    import("isolated_app/injector").then(({ inject }) => {
+      unmount = inject(elementId, { name: "main" });
+    });
+
+    return () => unmount();
+  }, []);
+
+  return (
+    <div className="container">
+      <div>Name: main-app</div>
+      <div>Framework: react</div>
+      <div>Language: TypeScript</div>
+      <div>CSS: Empty CSS</div>
+      <div id={elementId}></div>
+    </div>
+  );
+};
+
+createRoot(document.getElementById("app")!).render(<App />);
